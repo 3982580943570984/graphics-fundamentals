7b563eb2088a2fd7cda9b8f0bc48c0b76a7f04bc
3982580943570984 <svina-egor@rambler.ru>
2023-10-15 20:35:50 +0300

    Initialized basic game version

---
+++ 9. Game/source/classes/projectile.ts
@@ -0,0 +1,37 @@
+export class Projectile {
+  context: CanvasRenderingContext2D;
+  x: number;
+  y: number;
+  radius: number;
+  color: string;
+  velocity: { x: number; y: number };
+
+  constructor(
+    context: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    radius: number,
+    color: string,
+    velocity: { x: number; y: number }
+  ) {
+    this.context = context;
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    this.context.beginPath();
+    this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    this.context.fillStyle = this.color;
+    this.context.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+}
